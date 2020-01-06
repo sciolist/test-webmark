6ddb7e8bc98be6ca9d5c288949ec7c9d655a3910
jdp <sciolist.se@gmail.com>
2020-01-06 08:55:22 +0100

    update raze impl

--- configurations/crystal-raze/app.cr
+++ configurations/crystal-raze/app.cr
@@ -2,7 +2,7 @@ require "raze"
 require "db"
 require "pg"
 
-CONN = DB.open "postgres://#{ENV["PGUSER"]}:#{ENV["PGPASSWORD"]}@#{ENV["PGHOST"]}:#{ENV["PGPORT"]}/#{ENV["PGDATABASE"]}"
+CONN = DB.open "postgres://#{ENV["PGUSER"]}:#{ENV["PGPASSWORD"]}@#{ENV["PGHOST"]}:#{ENV["PGPORT"]}/#{ENV["PGDATABASE"]}?initial_pool_size=50&max_pool_size=50&max_idle_pool_size=50"
 
 get "/helloworld" do |ctx|
   "Hello, world"
@@ -10,16 +10,20 @@ end
 
 get "/10-fortunes" do |ctx|
   list = Array(NamedTuple(id: Int32, message: String)).new
-  CONN.query_each("select id, message from fortunes limit 10") do |rs|
+  CONN.using_connection do |conn|
+  conn.query_each("select id, message from fortunes limit 10") do |rs|
     list.push({ id: rs.read(Int32), message: rs.read(String) })
+  end
   end
   list.to_json
 end
 
 get "/all-fortunes" do |ctx|
   list = Array(NamedTuple(id: Int32, message: String)).new
-  CONN.query_each("select id, message from fortunes") do |rs|
+  CONN.using_connection do |conn|
+  conn.query_each("select id, message from fortunes") do |rs|
     list.push({ id: rs.read(Int32), message: rs.read(String) })
+  end
   end
   list.to_json
 end
--- configurations/crystal-raze/cluster.sh
+++ configurations/crystal-raze/cluster.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sleep 5
+cp shard.lock /src/shard.lock
 for i in $(seq 1 $(nproc --all)); do
   ./app &
 done
